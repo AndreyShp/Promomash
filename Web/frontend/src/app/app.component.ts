14789699f040ad1a4836d75cf4107d1802d7237b
Andrey <drey0785@gmail.com>
2019-10-16 15:39:28 +0300

    Подключил angular/material + черновик общего шага(не в отдельном контроле)

--- Web/frontend/src/app/app.component.ts
+++ Web/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webApp';
+
+  loginControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  passwordControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  confirmPasswordControl = new FormControl('', [
+    Validators.required
+  ]);
+  
+  agreeControl = new FormControl('', [
+    Validators.required
+  ]);
 }
